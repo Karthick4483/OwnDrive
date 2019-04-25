42e1a52b0e8b15e830e87a08b52692919cefc18d
karthikb <karthikb@analyticalwizards.com>
2019-04-25 11:39:02 +0530

    host name added

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ require('./config/mongoose');
 // module.parent check is required to support mocha watch
 // src: https://github.com/mochajs/mocha/issues/1912
 if (!module.parent) {
-  app.listen(config.port, () => {
+  app.listen(config.port, '0.0.0.1', () => {
     console.info(`server started on port ${config.port} (${config.env})`);
   });
 }
