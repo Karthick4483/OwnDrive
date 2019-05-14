a50c244c094b75d6bd3435e8f773420f3c2edee0
karthikb <karthikb@analyticalwizards.com>
2019-05-14 20:56:48 +0530

    register fix

--- server/config/express.js
+++ server/config/express.js
@@ -56,7 +56,7 @@ app.use('/scripts/', express.static(path.join(__dirname, `${distDir}/scripts`)))
 app.use('/build/', express.static(path.join(__dirname, distDir)));
 app.use('/api/', routes);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use('/register', () => res.redirect('/app/register'));
+// app.use('/register', () => res.redirect('/app/register'));
 app.use('/', views);
 
 app.use((req, res, next) => {
